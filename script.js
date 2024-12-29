6fd4faa1ac91a3db7af63e040983c8d42e5e3373
pingwiner <i486dx@yandex.ru>
2024-12-30 00:28:02 +0400

    2d algorithms

--- script.js
+++ script.js
@@ -2,16 +2,33 @@ const width = 32
 const height = 16;
 const cellSize = 10;
 const canvas = document.getElementById("canvas");
-const start = document.getElementById("start");
+const startButton = document.getElementById("start");
+const clearButton = document.getElementById("clear");
+const randomButton = document.getElementById("random");
+const algorithm = document.getElementById("algorithm");
 
 let field = Array(width * height);
+let interval = null;
+let selectedAlgorithm = conways_game_of_life;
+let algos = [
+	conways_game_of_life, 
+	function() { rule(110) },
+	function() { rule(54) }
+];
 
-function init() {
+function getRandom() {
+  return Math.floor(Math.random() * 2);
+}
+
+function clear() {
 	for (let i = 0; i < height; i+= 1) {
 		for (let j = 0; j < width; j+= 1) {
-			field[i * width + j] = j % 2;
+			field[i * width + j] = 0;
 		}
 	}	
+}
+
+function init() {
 	canvas.addEventListener("click", function(e) {
 		let x = Math.floor(event.clientX / cellSize) - 1;
 		let y = Math.floor(event.clientY / cellSize) - 1;
@@ -19,9 +36,30 @@ function init() {
 		field[y * width + x] = (v == 1) ? 0 : 1;
 		draw();
 	});
-	start.addEventListener("click", function(e) {
-		setInterval(tick, 1000);
+	startButton.addEventListener("click", function(e) {
+		if (interval == null) {
+			setAlgorithm();
+			interval = setInterval(tick, 1000);
+			startButton.innerText= "Stop";
+		} else {
+			clearInterval(interval);
+			interval = null;
+			startButton.innerText= "Start";
+		}
 	});	
+	clearButton.addEventListener("click", function(e) {
+		clear();
+		draw();			
+	});	
+	randomButton.addEventListener("click", function(e) {
+		for (let i = 0; i < height; i+= 1) {
+			for (let j = 0; j < width; j+= 1) {
+				field[i * width + j] = getRandom();
+			}
+		}	
+		draw();
+	});
+	clear();
 }
 
 function draw() {
@@ -56,27 +94,66 @@ function neigboursCount(y, x) {
 			get(y + 1, x - 1) + get(y + 1, x) + get(y + 1, x + 1);
 }
 
-function cellFunc(y, x) {
-	let neigbours = neigboursCount(y, x);
-	if (neigbours < 2) return 0;
-	if (neigbours > 3) return 0;
-	let currentState = get(y, x);
-	if (currentState == 0) {
-		if (neigbours == 3) return 1;
-		return 0;
-	}
-	return currentState;
+function tick() {
+	draw();
+	selectedAlgorithm();
 }
 
-function tick() {
+function setAlgorithm() {
+	let value = algorithm.value;
+	index = parseInt(value);
+	selectedAlgorithm = algos[index];
+}
+
+function conways_game_of_life() {
+	let cellFunc = function(y, x) {
+		let neigbours = neigboursCount(y, x);
+		if (neigbours < 2) return 0;
+		if (neigbours > 3) return 0;
+		let currentState = get(y, x);
+		if (currentState == 0) {
+			if (neigbours == 3) return 1;
+			return 0;
+		}
+		return currentState;
+	}
+
 	let newField = Array(width * height);
 	for (let i = 0; i < height; i+= 1) {
 		for (let j = 0; j < width; j+= 1) {
 			newField[i * width + j] = cellFunc(i, j);
 		}
 	}
-	field = newField;	
-	draw();
+	field = newField;		
+}
+
+function intToArray(x) {
+	let result = [];
+	while(result.length < 8) {
+		result.push(x % 2);
+		x = Math.floor(x / 2);
+	}
+	return result;
+} 
+
+function rule(x) {
+	let values = intToArray(x);	
+	let cellFunc = function(y, x) {
+		if (y == 0) return get(y, x);
+		let index =  get(y - 1, x - 1) * 4 +
+					get(y - 1, x) * 2 +
+					get(y - 1, x + 1);
+		return values[index];
+	}
+
+	let newField = Array(width * height);
+
+	for (let i = 0; i < height; i+= 1) {
+		for (let j = 0; j < width; j+= 1) {
+			newField[i * width + j] = cellFunc(i, j);
+		}
+	}
+	field = newField;		
 }
 
 init();
